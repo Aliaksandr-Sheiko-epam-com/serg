e398a25258367291a2aacde7ee3a8afe211cff65
sasha <sheiko.an@gmail.com>
2016-01-31 15:59:30 +0300

    Insert method implemented

--- binary-tree/binary-tree.js
+++ binary-tree/binary-tree.js
@@ -7,7 +7,11 @@ class BinaryTree {
 	}
 
 	insert(data) {
-
+		if (this.root == null) {
+			this.root = new Node(data, null, null);
+		} else {
+			this.insertNode(data, this.root);
+		}
 	}
 
 	contains(data) {
@@ -24,5 +28,21 @@ class BinaryTree {
 
 	isEmpty() {
 
+	}
+	
+	insertNode(data, currentNode) {
+		if (data < currentNode.data) {
+			if (currentNode.left == null) {
+				currentNode.left = new Node(data, null, null);
+			} else {
+				this.insertNode(data, currentNode.left);
+			}
+		} else {
+			if (currentNode.right == null) {
+				currentNode.right = new Node(data, null, null);
+			} else {
+				this.insertNode(data, currentNode.right);
+			}
+		}
 	}
 }
